2db4cfe61ff91dcec3927222904f102f744e1e68
Avi-020 <avipawar020@gmail.com>
2023-07-29 17:54:35 +0530

    images

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import WhatsappLOGO from "../images/WhatsappLOGO.png"
+import * as mdb from 'mdb-ui-kit'; // lib
+import { Input } from 'mdb-ui-kit'; // module
+// @import '~mdb-ui-kit/css/mdb.min.css';
+
 
 function HomePage() {
   return (
@@ -41,14 +45,126 @@ function HomePage() {
 
 
 
+{/* <!-- Carousel wrapper --> */}
+<div
+  id="carouselBasicExample"
+  className="carousel slide carousel-fade"
+  data-mdb-ride="carousel"
+>
+  {/* <!-- Indicators --> */}
+  <div className="carousel-indicators">
+    <button
+      type="button"
+      data-mdb-target="#carouselBasicExample"
+      data-mdb-slide-to="0"
+      className="active"
+      aria-current="true"
+      aria-label="Slide 1"
+    ></button>
+    <button
+      type="button"
+      data-mdb-target="#carouselBasicExample"
+      data-mdb-slide-to="1"
+      aria-label="Slide 2"
+    ></button>
+    <button
+      type="button"
+      data-mdb-target="#carouselBasicExample"
+      data-mdb-slide-to="2"
+      aria-label="Slide 3"
+    ></button>
+  </div>
+
+  {/* <!-- Inner --> */}
+  <div className="carousel-inner">
+    {/* <!-- Single item --> */}
+    <div className="carousel-item active">
+      <img
+        src="https://s3.ap-south-1.amazonaws.com/townscript-production/images/8eb2c690-f830-48de-ba88-e40adbe844f8.jpg"
+        className="d-block w-100"
+        alt="Sunset Over the City"
+      />
+      <div className="carousel-caption d-none d-md-block">
+        <h5>PAWANA LAKE SUNSHINE CAMPING</h5>
+        
+      </div>
+    </div>
+
+    {/* <!-- Single item --> */}
+    <div className="carousel-item">
+      <img
+        src="https://www.tripadvisortour.com/wp-content/uploads/2022/03/Pawna-Camping-6.jpeg"
+        className="d-block w-100"
+        alt="Canyon at Nigh"
+      />
+      <div className="carousel-caption d-none d-md-block">
+      <h5>PAWANA LAKE SUNSHINE CAMPING</h5>
+
+      </div>
+    </div>
+
+    {/* <!-- Single item --> */}
+    <div className="carousel-item">
+      <img
+        src="https://roundtheworld.in/wp-content/uploads/2021/05/Pawna-lake-Camp-J-06.jpeg"
+        className="d-block w-100"
+        alt="Cliff Above a Stormy Sea"
+      />
+      <div className="carousel-caption d-none d-md-block">
+      <h5>PAWANA LAKE SUNSHINE CAMPING</h5>
+
+      </div>
+    </div>
+  </div>
+  {/* <!-- Inner -->
+
+  <!-- Controls --> */}
+  <button
+    className="carousel-control-prev"
+    type="button"
+    data-mdb-target="#carouselBasicExample"
+    data-mdb-slide="prev"
+  >
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button
+    className="carousel-control-next"
+    type="button"
+    data-mdb-target="#carouselBasicExample"
+    data-mdb-slide="next"
+  >
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+{/* <!-- Carousel wrapper --> */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* footer of whatsapp */}
 <div className='whatsapp' style={{textAlign:"end"}}>
 
       <a href="https://wa.me/9067661322">
         <img alt="Chat on WhatsApp" src={WhatsappLOGO} style={{width:"20vh", height:"20vh",position:"sticky"}} />
       </a>
 </div>
-
-
     </>
   )
 }
