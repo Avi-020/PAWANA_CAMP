9e408805e6ab617969f8f1aa338cc337829630cd
Avi-020 <avipawar020@gmail.com>
2023-07-30 19:58:23 +0530

    some changes

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -3,6 +3,7 @@ import WhatsappLOGO from "../images/WhatsappLOGO.png"
 import * as mdb from 'mdb-ui-kit'; // lib
 import { Input } from 'mdb-ui-kit'; // module
 // @import '~mdb-ui-kit/css/mdb.min.css';
+import schedule from "../images/s4.svg"
 
 
 function HomePage() {
@@ -48,7 +49,7 @@ function HomePage() {
 {/* <!-- Carousel wrapper --> */}
 <div
   id="carouselBasicExample"
-  className="carousel slide carousel-fade"
+  className="carousel slide carousel-fade mx-2"
   data-mdb-ride="carousel"
 >
   {/* <!-- Indicators --> */}
@@ -140,11 +141,35 @@ function HomePage() {
 </div>
 {/* <!-- Carousel wrapper --> */}
 
-
-
+<p className="ms-3 mt-3"> Looking for an extreme lakeside camping experience&#63; Where you
+              will be surrounded by serene nature and forts around the lake&#63;
+              We are offering you the convenience to explore Pawna lake. Pawna
+              lake the most alluring camping spot in Lonavala. It is 20
+              kilometers from Lonavala city center. 110 kilometers from Mumbai
+              city&sbquo; and 58 kilometers from Pune city. We are offering you
+              the best experience at SUNSHINE CAMPING. We provide a tent
+              stay&sbquo; unlimited dinner&sbquo; free snacks&sbquo; and
+              delicious barbecue. Not only that&sbquo; but we provide live
+              guitar music on specific day to soothe your evening. A campfire
+              and adventure activities will add more fun to your trip.</p>
 <div style={{textAlign:"center",marginTop:"2vh"}}>
 
-<h3>Pakages</h3>
+
+<h2 style={{textAlign:"center"}} >SUNSHINE CAMPING</h2>
+<h4 style={{textAlign:"center",color:"rgba(17, 14, 14, 0.36)"}}>SCHEDULE</h4>
+<div className="schedule">
+
+<img src={schedule} alt="schedule"  style={{width:"100%",height:"auto"}}/>
+
+
+
+</div>
+
+
+
+
+
+<h3 className='mt-5' style={{textAlign:"center"}}>Pakages</h3>
 </div>
 
 
@@ -168,33 +193,15 @@ function HomePage() {
           <div className="col-lg-4">
             <div className="card">
               <img
-                src="https://mdbcdn.b-cdn.net/img/new/standard/nature/181.webp"
+                src="https://pawnacamp.com/storage/2018/11/Pawna-lake-camping-i-display-picture.jpg"
                 className="card-img-top"
                 alt="Waterfall"
               />
               <div className="card-body">
-                <h5 className="card-title">Card title</h5>
+                <h5 className="card-title">SUNSHINE CAMPING</h5>
                 <p className="card-text">
-                  Some quick example text to build on the card title and make up the bulk
-                  of the cards content.
-                </p>
-                <a href="#!" className="btn btn-primary">Button</a>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-lg-4 d-none d-lg-block">
-            <div className="card">
-              <img
-                src="https://mdbcdn.b-cdn.net/img/new/standard/nature/182.webp"
-                className="card-img-top"
-                alt="Sunset Over the Sea"
-              />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up the bulk
-                  of the cards content.
+                 Starting From
+                <strike> ₹ 1&sbquo;500</strike> 1&sbquo;200
                 </p>
                 <a href="#!" className="btn btn-primary">Book Now</a>
               </div>
@@ -204,15 +211,33 @@ function HomePage() {
           <div className="col-lg-4 d-none d-lg-block">
             <div className="card">
               <img
-                src="https://mdbcdn.b-cdn.net/img/new/standard/nature/183.webp"
+                src="https://pawnacamp.com/storage/2017/11/Pawnacamp-D-location-Aug-28-2022-08-14-AM-01.jpg"
+                className="card-img-top"
+                alt="Sunset Over the Sea"
+              />
+              <div className="card-body">
+                <h5 className="card-title">SUNSHINE CAMPING</h5>
+                <p className="card-text">
+                 Starting From
+                <strike> ₹ 1&sbquo;500</strike> 1&sbquo;200
+                </p>
+                <a href="#!" className="btn btn-primary">Book Now</a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-4 d-none d-lg-block">
+            <div className="card">
+              <img
+                src="https://pawnacamp.com/storage/2022/09/Pawna-camp-C-Family-camp-01.jpg"
                 className="card-img-top"
                 alt="Sunset over the Sea"
               />
               <div className="card-body">
-                <h5 className="card-title">Card title</h5>
+                <h5 className="card-title">SUNSHINE CAMPING</h5>
                 <p className="card-text">
-                  Some quick example text to build on the card title and make up the bulk
-                  of the cards content.
+                 Starting From
+                <strike> ₹ 1&sbquo;500</strike> 1&sbquo;200
                 </p>
                 <a href="#!" className="btn btn-primary">Book Now</a>
               </div>
@@ -229,15 +254,15 @@ function HomePage() {
           <div className="col-lg-4 col-md-12">
             <div className="card">
               <img
-                src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp"
+                src="https://pawnacamp.com/storage/2018/11/Pawna-lake-camping-camp-F-09.jpg"
                 className="card-img-top"
                 alt="Fissure in Sandstone"
               />
               <div className="card-body">
-                <h5 className="card-title">Card title</h5>
+                <h5 className="card-title">SUNSHINE CAMPING</h5>
                 <p className="card-text">
-                  Some quick example text to build on the card title and make up the bulk
-                  of the cards content.
+                 Starting From
+                <strike> ₹ 1&sbquo;500</strike> 1&sbquo;200
                 </p>
                 <a href="#!" className="btn btn-primary">Book Now</a>
               </div>
@@ -247,15 +272,15 @@ function HomePage() {
           <div className="col-lg-4 d-none d-lg-block">
             <div className="card">
               <img
-                src="https://mdbcdn.b-cdn.net/img/new/standard/nature/185.webp"
+                src="https://campatpawnalake.com/cdn/shop/products/WhatsAppImage2022-11-17at3.55.30PM.jpg?v=1668699011"
                 className="card-img-top"
                 alt="Storm Clouds"
               />
               <div className="card-body">
-                <h5 className="card-title">Card title</h5>
+                <h5 className="card-title">SUNSHINE CAMPING</h5>
                 <p className="card-text">
-                  Some quick example text to build on the card title and make up the bulk
-                  of the cards content.
+                 Starting From
+                <strike> ₹ 1&sbquo;500</strike> 1&sbquo;200
                 </p>
                 <a href="#!" className="btn btn-primary">Book Now</a>
               </div>
@@ -265,15 +290,15 @@ function HomePage() {
           <div className="col-lg-4 d-none d-lg-block">
             <div className="card">
               <img
-                src="https://mdbcdn.b-cdn.net/img/new/standard/nature/186.webp"
+                src="https://pawnacamp.com/storage/2017/09/WhatsApp-Image-2022-12-24-at-3.07.48-PM-683x455.jpeg"
                 className="card-img-top"
                 alt="Hot Air Balloons"
               />
               <div className="card-body">
-                <h5 className="card-title">Card title</h5>
+                <h5 className="card-title">SUNSHINE CAMPING</h5>
                 <p className="card-text">
-                  Some quick example text to build on the card title and make up the bulk
-                  of the cards content.
+                 Starting From
+                <strike> ₹ 1&sbquo;500</strike> 1&sbquo;200
                 </p>
                 <a href="#!" className="btn btn-primary">Book Now</a>
               </div>
@@ -290,15 +315,15 @@ function HomePage() {
           <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
             <div className="card">
               <img
-                src="https://mdbcdn.b-cdn.net/img/new/standard/nature/187.webp"
+                src="https://vl-prod-static.b-cdn.net/system/images/000/642/528/f2c0e59271be20889630cecf76cebe3b/original/80be8ad9-4ec3-44f6-bb86-788b6700ad22.jpg?1665474765"
                 className="card-img-top"
                 alt="Peaks Against the Starry Sky"
               />
               <div className="card-body">
-                <h5 className="card-title">Card title</h5>
+                <h5 className="card-title">SUNSHINE CAMPING</h5>
                 <p className="card-text">
-                  Some quick example text to build on the card title and make up the bulk
-                  of the cards content.
+                 Starting From
+                <strike> ₹ 1&sbquo;500</strike> 1&sbquo;200
                 </p>
                 <a href="#!" className="btn btn-primary">Book Now</a>
               </div>
@@ -308,15 +333,15 @@ function HomePage() {
           <div className="col-lg-4 mb-4 mb-lg-0 d-none d-lg-block">
             <div className="card">
               <img
-                src="https://mdbcdn.b-cdn.net/img/new/standard/nature/188.webp"
+                src="https://pawnalakecamps.in/wp-content/uploads/2023/01/IMG-20230104-WA0005.jpg"
                 className="card-img-top"
                 alt="Bridge Over Water"
               />
               <div className="card-body">
-                <h5 className="card-title">Card title</h5>
+                <h5 className="card-title">SUNSHINE CAMPING</h5>
                 <p className="card-text">
-                  Some quick example text to build on the card title and make up the bulk
-                  of the cards content.
+                  Starting From
+                <strike> ₹ 1&sbquo;500</strike> 1&sbquo;200
                 </p>
                 <a href="#!" className="btn btn-primary">Book Now</a>
               </div>
@@ -326,15 +351,15 @@ function HomePage() {
           <div className="col-lg-4 mb-4 mb-lg-0 d-none d-lg-block">
             <div className="card">
               <img
-                src="https://mdbcdn.b-cdn.net/img/new/standard/nature/189.webp"
+                src="https://pawnacamp.com/storage/2017/11/Pawna-lake-camping-camp-D-10-1.jpg"
                 className="card-img-top"
                 alt="Purbeck Heritage Coast"
               />
               <div className="card-body">
-                <h5 className="card-title">Card title</h5>
+                <h5 className="card-title">SUNSHINE CAMPING</h5>
                 <p className="card-text">
-                  Some quick example text to build on the card title and make up the bulk
-                  of the cards content.
+                 Starting From
+                <strike> ₹ 1&sbquo;500</strike> 1&sbquo;200
                 </p>
                 <a href="#!" className="btn btn-primary">Book Now</a>
               </div>
@@ -373,8 +398,68 @@ function HomePage() {
 
 
 
+<div className="table" style={{paddingLeft:"4vh",paddingRight:"4vh",marginBottom:"5vh"}}>
 
 
+      <table className="table">
+  <thead className="thead-light">
+    <tr>
+      <th scope="col" style={{textAlign:"center"}}>REGULAR CAMP</th>
+      <th scope="col" style={{textAlign:"center"}}>DELUX CAMP</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr style={{textAlign:"center"}}>
+      <td>₹1199 per person</td>
+      <td>₹1499 per person</td>
+    </tr>
+    <tr style={{textAlign:"center"}}>
+      <td>4PM to next day 11AM</td>
+      <td>4PM to next day 11AM</td>
+    </tr>
+    <tr style={{textAlign:"center"}}>
+      <td>Tea&sbquo; coffee&sbquo; poha in breakfast</td>
+      <td>Tea&sbquo; coffee&sbquo; poha&sbquo; bread&sbquo; jam&sbquo; anda bhurji in breakfast</td>
+    </tr>
+    <tr style={{textAlign:"center"}}>
+      <td>No live guitar show</td>
+      <td>Live guitar on every Saturday</td>
+    </tr>
+    <tr style={{textAlign:"center"}}>
+      <td>2/3 person sharing tents</td>
+      <td>2/3/4 person sharing tents</td>
+    </tr>
+    <tr style={{textAlign:"center"}}>
+      <td>200g BBQ per person
+(Veg/Nveg)</td>
+      <td>250g BBQ per person
+(Veg/Nveg)</td>
+    </tr>
+    <tr style={{textAlign:"center"}}>
+      <td>Unlimited dinner</td>
+      <td>Unlimited dinner</td>
+    </tr>
+    <tr style={{textAlign:"center"}}>
+      <td>Common campfire</td>
+      <td>Common campfire</td>
+    </tr>
+    <tr style={{textAlign:"center"}}>
+      <td>Trolley speaker</td>
+      <td>Trolley speaker</td>
+    </tr>
+    <tr style={{textAlign:"center"}}>
+      <td>Free parking</td>
+      <td>Free parking</td>
+    </tr>
+    <tr style={{textAlign:"center"}}>
+      <td>Games: Archery&sbquo; Dart game&sbquo; Cricket&sbquo; Football&sbquo; Badminton&sbquo; Carrom&sbquo; Volleyball and more</td>
+      <td>Games: Archery&sbquo; Dart game&sbquo; Cricket&sbquo; Football&sbquo; Badminton&sbquo; Carrom&sbquo; Volleyball and more</td>
+    </tr>
+  </tbody>
+</table>
+
+
+</div>
 
 
 {/* footer of whatsapp */}
